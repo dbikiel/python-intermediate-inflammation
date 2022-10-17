caca0122cf15d07927b8b04f6f6bfb0e47a2e5b9
Damian Bikiel <damian.bikiel@astrazeneca.com>
2022-10-17 12:54:38 -0400

    added classes Person, Patient, Doctor

--- inflammation/models.py
+++ inflammation/models.py
@@ -82,4 +82,55 @@ def daily_std(data):
     """
     return np.std(data, axis=0)
 
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+
+class Doctor(Person):
+    """A Doctor in a clinical trial."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient):
+        if patient not in self.patients:
+            self.patients.add(patient)
+
+    def get_patients(self):
+        return self.patients
+
+
 
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,22 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Alice'
+    d = Doctor(name=name)
+
+    assert d.name == name
+
+def test_add_patients_to_doctor():
+    from inflammation.models import Patient, Doctor
+
+    name = 'Alice'
+    p = Patient(name=name)
+
+    name = 'Jane'
+    d = Doctor(name=name)
+    d.add_patient(p)
+    assert d.get_patients[0] == p
